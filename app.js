7f3cc2091f24ed3dd2996db271fe348962d20ba6
Sarah Hershenhouse <sarahhershey99@gmail.com>
2023-11-27 22:20:46 -0700

    completed-exercise

--- app.js
+++ app.js
@@ -3,8 +3,31 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Sarah"; 
+let lastName = "Hershenhouse";
+var age = 24;
 
-// Exercise 2
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
 
-// Exercise 3
+console.log(`${language} was first released in ${createdYear}.`);
+
+let product = "T-Shirt";
+let price = 19.99;
+let isOnSale = false;
+let salesPercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`${product} in size ${selectedSize} is $${price} with ${stock} in stock.`)
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} is a book by ${author}. The book is ${pageCount} pages long, 
+and your last bookmark was on page ${bookmark}.`)
